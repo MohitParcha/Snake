a141fe4ae12c265c7241ba23ed24915de0bb883f
MohitParcha <99893212+MohitParcha@users.noreply.github.com>
2022-11-30 21:14:35 +0530

    Initial commit

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    public BoxCollider2D gridArea;
+    private void Start()
+    {
+        RandomizePosition();
+    }
+
+    public void RandomizePosition()
+    {
+        Bounds bounds = gridArea.bounds;
+
+        // Pick a random position inside the bounds
+        float x = Random.Range(bounds.min.x, bounds.max.x);
+        float y = Random.Range(bounds.min.y, bounds.max.y);
+
+        // Round the values to ensure it aligns with the grid
+        x = Mathf.Round(x);
+        y = Mathf.Round(y);
+
+        transform.position = new Vector2(x, y);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            RandomizePosition();
+        }
+    }
+
+}
